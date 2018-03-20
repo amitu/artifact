bebc85a3d222f76eb9c202e653ffd40a70b892f6
Garrett Berg <googberg@gmail.com>
2018-03-19 21:45:26 -0600

    getting closer to artifact-lib

---
+++ artifact-lib/src/lib.rs
@@ -0,0 +1,426 @@
+extern crate base64;
+#[macro_use]
+extern crate expect_macro;
+extern crate failure;
+#[macro_use]
+extern crate failure_derive;
+#[macro_use]
+extern crate ergo_std;
+extern crate ergo_config;
+#[macro_use]
+extern crate ordermap;
+extern crate path_abs;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate siphasher;
+
+use std::fmt;
+use std::result;
+use siphasher::sip128::{Hasher128, SipHasher};
+use ergo_std::*;
+use ergo_config::*;
+use path_abs::*;
+
+mod name;
+pub mod lint;
+
+pub use name::{Name, SubName, Type};
+
+
+
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
+/// #SPC-read-structs.artifact
+/// The primary data structure of this library which encapsulates a majority of the useful
+/// end product of a user's project.
+pub struct Artifact {
+    /// The hahs-id of this artifact. This is required for modifying artifacts.
+    pub id: HashIm,
+    /// The name of the artifact.
+    ///
+    /// While this library uses `Name` as the key, other libraries (like a web-ui)
+    /// might not. This also makes it much simpler to reserialize artifacts as
+    /// the mapping cannot be broken.
+    pub name: Name,
+    /// The file the artifact is defined in.
+    pub file: PathArc,
+    /// The user defined and calculated `partof` the artifact.
+    pub partof: OrderSet<Name>,
+    /// The (calculated) parts of the artifact (opposite of partof)
+    pub parts: OrderSet<Name>,
+    /// The (calculated) completion+tested ratios of the artifact.
+    pub completed: Completed,
+    /// The user defined text
+    pub text: String,
+    /// Whether the artifact is implemented directly (in code or `done` field)
+    pub impl_: Impl,
+    /// Subnames in the text.
+    pub subnames: OrderSet<SubName>,
+}
+
+impl Artifact {
+    pub fn sort(&mut self) {
+        self.partof.sort();
+        self.parts.sort();
+        if let Impl::Code(ref mut c) = self.impl_ {
+            c.secondary.sort_keys();
+        }
+        self.subnames.sort();
+    }
+}
+
+// ----- IMPL -----
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(tag="type", content="value")]
+/// Encapsulates the implementation state of the artifact
+pub enum Impl {
+    /// The artifact is "defined as done"
+    Done(String),
+    /// The artifact is at least partially implemented in code.
+    Code(ImplCode),
+    /// The artifact is not implemented directly at all
+    NotImpl,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+/// Encapsulates the implementation state of the artifact in code.
+pub struct ImplCode {
+    pub primary: Option<CodeLoc>,
+    pub secondary: OrderMap<SubName, CodeLoc>,
+}
+
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
+/// The location of an artifact reference in code.
+pub struct CodeLoc {
+    pub file: PathFile,
+    pub line: u64,
+}
+
+impl CodeLoc {
+    pub fn new(file: &PathFile, line: u64) -> CodeLoc {
+        CodeLoc {
+            file: file.clone(),
+            line: line,
+        }
+    }
+}
+
+impl Impl {
+    /// Return the `(count, value, secondary_count, secondary_value)`
+    /// that this impl should contribute to the "implemented" statistics.
+    ///
+    /// "secondary" is used because the Done field actually does contribute to
+    /// both spc AND tst for REQ and SPC types.
+    ///
+    /// `subnames` should contain the subnames that exist in that artifact's text
+    pub(crate) fn to_statistics(&self, subnames: &OrderSet<SubName>) -> (usize, f64, usize, f64) {
+        match *self {
+            Impl::Done(_) => (1, 1.0, 1, 1.0),
+            Impl::Code(ref impl_) => {
+                let mut count = 1;
+                let mut value = f64::from(impl_.primary.is_some() as u8);
+                for sub in subnames.iter() {
+                    count += 1;
+                    // add 1 if the subname is implemented, else 0
+                    value += f64::from(impl_.secondary.contains_key(sub) as u8);
+                }
+                (count, value, 0, 0.0)
+            }
+            Impl::NotImpl => {
+                if !subnames.is_empty() {
+                    // If subnames are defined not being implemented
+                    // in code means that you get counts against you
+                    (1 + subnames.len(), 0.0, 0, 0.0)
+                } else {
+                    (0, 0.0, 0, 0.0)
+                }
+            }
+        }
+    }
+
+    /// Return whether this is the `Done` variant.
+    pub fn is_done(&self) -> bool {
+        match *self {
+            Impl::Done(_) => true,
+            _ => false,
+        }
+    }
+}
+
+impl fmt::Display for Impl {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Impl::Done(ref s) => write!(f, "{}", s),
+            Impl::Code(ref c) => write!(f, "{}", c),
+            Impl::NotImpl => write!(f, "not directly implemented"),
+        }
+    }
+}
+
+impl fmt::Display for ImplCode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if let Some(ref loc) = self.primary {
+            write!(f, "{:?}", loc)?;
+        }
+        if !self.secondary.is_empty() {
+            write!(f, "Secondary{:?}", self.secondary)?;
+        }
+        Ok(())
+    }
+}
+
+impl fmt::Debug for CodeLoc {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}[{}]", self.file.display(), self.line)
+    }
+}
+
+
+// ----- INTERMEDIATE -----
+
+/// The type used for unique hash ids
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
+pub struct HashIm(pub(crate) [u8; 16]);
+
+#[derive(Debug, Serialize, Deserialize)]
+/// #SPC-structs.artifact_im
+pub struct ArtifactIm {
+    pub name: Name,
+    pub file: PathArc,
+    pub partof: OrderSet<Name>,
+    pub done: Option<String>,
+    pub text: String,
+}
+
+// IMPL ArtifactIm
+
+impl ArtifactIm {
+    /// Create the intermediate hash.
+    ///
+    /// This is the primary key used to ensure consistency when modifying artifats via an API.
+    pub fn hash_im(&self) -> HashIm {
+        let mut hasher = SipHasher::new();
+        self.hash(&mut hasher);
+        HashIm(hasher.finish128().as_bytes())
+    }
+
+    // FIXME
+    // /// Process the `ArtifactIm`.
+    // ///
+    // /// This is required whenever serializing/deserializing the ArtifactIm.
+    // pub fn clean(&mut self) {
+    //     family::strip_auto_partofs(&self.name, &mut self.partof);
+    //     self.partof.sort();
+    //     raw::clean_text(&mut self.text);
+    // }
+
+}
+
+impl From<Artifact> for ArtifactIm {
+    /// Get an `ArtifactIm` from an `Artifact`
+    fn from(art: Artifact) -> ArtifactIm {
+        let mut out = ArtifactIm {
+            name: art.name,
+            file: art.file.into(),
+            partof: art.partof,
+            done: match art.impl_ {
+                Impl::Done(d) => Some(d),
+                _ => None,
+            },
+            text: art.text,
+        };
+        out.clean();
+        out
+    }
+}
+
+impl Hash for ArtifactIm {
+    /// Normal hash **except** we use `name.as_str().hash()` instead of
+    /// `name.hash()` to record whether the raw name itself changed.
+    ///
+    /// Note: normally name is hashed by its type and key.
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.name.as_str().hash(state);
+        self.file.hash(state);
+        // note: guaranteed that it is always stripped and sorted
+        for p in self.partof.iter() {
+            p.hash(state);
+        }
+        self.done.hash(state);
+        self.text.hash(state);
+    }
+}
+
+impl fmt::Display for HashIm {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", expect!(json::to_string(&self)))
+    }
+}
+
+impl fmt::Debug for HashIm {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+impl Default for HashIm {
+    fn default() -> HashIm {
+        HashIm([0; 16])
+    }
+}
+
+impl Serialize for HashIm {
+    fn serialize<S>(&self, serializer: S) -> result::Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let b64 = base64::encode_config(&self.0, base64::URL_SAFE_NO_PAD);
+        serializer.serialize_str(&b64)
+    }
+}
+
+impl<'de> Deserialize<'de> for HashIm {
+    fn deserialize<D>(deserializer: D) -> result::Result<HashIm, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let b64 = String::deserialize(deserializer)?;
+        let mut hash = [0_u8; 16];
+        base64::decode_config_slice(&b64, base64::URL_SAFE_NO_PAD, &mut hash)
+            .map_err(serde::de::Error::custom)?;
+        Ok(HashIm(hash))
+    }
+}
+
+// ------ OPERATIONS -----
+
+#[derive(Debug, Serialize, Deserialize)]
+/// #SPC-structs.artifact_op
+/// Used for specifying operations to perform.
+pub enum ArtifactOp {
+    Create {
+        artifact: ArtifactIm,
+    },
+    Update {
+        artifact: ArtifactIm,
+        orig_id: HashIm,
+    },
+    Delete {
+        name: Name,
+        orig_id: HashIm,
+    },
+}
+
+struct IdPieces {
+    name: Name,
+    orig_id: Option<HashIm>,
+    new_id: Option<HashIm>,
+}
+
+impl ArtifactOp {
+    pub(crate) fn clean(&mut self) {
+        match *self {
+            ArtifactOp::Create { ref mut artifact }
+            | ArtifactOp::Update {
+                ref mut artifact, ..
+            } => artifact.clean(),
+            _ => {}
+        }
+    }
+
+    fn id_pieces(&self) -> IdPieces {
+        match *self {
+            ArtifactOp::Create { ref artifact } => IdPieces {
+                name: artifact.name.clone(),
+                orig_id: None,
+                new_id: Some(artifact.hash_im()),
+            },
+            ArtifactOp::Update {
+                ref artifact,
+                ref orig_id,
+            } => IdPieces {
+                name: artifact.name.clone(),
+                orig_id: Some(*orig_id),
+                new_id: Some(artifact.hash_im()),
+            },
+            ArtifactOp::Delete {
+                ref name,
+                ref orig_id,
+            } => IdPieces {
+                name: name.clone(),
+                orig_id: Some(*orig_id),
+                new_id: None,
+            },
+        }
+    }
+}
+
+// ----- SETTINGS -----
+
+#[derive(Debug, Eq, PartialEq, Clone, Serialize, Deserialize)]
+pub(crate) struct SettingsRaw {
+    pub artifact_paths: Vec<String>,
+    pub exclude_artifact_paths: Vec<String>,
+    pub code_paths: Vec<String>,
+    pub exclude_code_paths: Vec<String>,
+}
+
+pub(crate) struct FoundPaths {
+    pub files: Vec<PathFile>,
+    pub dirs: Vec<PathAbs>,
+}
+
+impl FoundPaths {
+    pub(crate) fn new() -> FoundPaths {
+        FoundPaths {
+            files: Vec::new(),
+            dirs: Vec::new(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+/// Paths that have have be recursively loaded.
+pub struct ProjectPaths {
+    pub base: PathDir,
+    pub code_paths: OrderSet<PathAbs>,
+    pub exclude_code_paths: OrderSet<PathAbs>,
+    pub artifact_paths: OrderSet<PathAbs>,
+    pub exclude_artifact_paths: OrderSet<PathAbs>,
+}
+
+// ------ COMPLETED ------
+
+#[derive(Debug, Default, Clone, PartialEq, PartialOrd, Copy, Serialize, Deserialize)]
+/// #SPC-read-structures.completed
+pub struct Completed {
+    /// The specification completion ratio.
+    pub spc: f32,
+    /// The tested completion ratio.
+    pub tst: f32,
+}
+
+
+// ------ PROJECT ------
+
+#[derive(Debug, PartialEq, Serialize)]
+pub struct Project {
+    pub paths: ProjectPaths,
+    pub code_impls: OrderMap<Name, ImplCode>,
+    pub artifacts: OrderMap<Name, Artifact>,
+}
+
+impl Project {
+    /// Recursively sort all the items in the project.
+    pub fn sort(&mut self) {
+        self.code_impls.sort_keys();
+        for (_, code) in self.code_impls.iter_mut() {
+            code.secondary.sort_keys();
+        }
+        self.artifacts.sort_keys();
+        for (_, art) in self.artifacts.iter_mut() {
+            art.sort();
+        }
+    }
+}
---
+++ artifact-lib/src/lint.rs
@@ -0,0 +1,190 @@
+/*  artifact: the requirements tracking tool made for developers
+ * Copyright (C) 2017  Garrett Berg <@vitiral, vitiral@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * */
+/// #SPC-lint
+/// Artifact lint types.
+///
+/// This is the primary error type for all "non fatal" errors and warnings.
+use std::sync::mpsc::Sender;
+use std::error;
+use std::fmt;
+
+use ergo_std::*;
+use ergo_config::*;
+use path_abs::*;
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+/// An artifact lint error or warning
+pub struct Lint {
+    pub level: Level,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub path: Option<PathArc>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub line: Option<u64>,
+    pub category: Category,
+    pub msg: String,
+}
+
+#[derive(Debug, Default, PartialEq, Serialize, Deserialize)]
+/// Categorized lints
+pub struct Categorized {
+    pub error: Vec<Lint>,
+    pub other: Vec<Lint>,
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+/// The lint level, error will eventually stop the program.
+pub enum Level {
+    Error,
+    Warn,
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+/// Where the lint is from
+pub enum Category {
+    LoadPaths,
+    ParseCodeImplementations,
+    ParseArtifactFiles,
+    AutoPartof,
+    Artifact,
+    ImplCode,
+    ModifyPathInvalid,
+    CreateExists,
+    UpdateNoop,
+    UpdateDne,
+    DeleteDne,
+    IdOverlap,
+    CreateBackups,
+    SaveProject,
+    RemoveBackups,
+}
+
+impl Categorized {
+    pub fn categorize<I>(&mut self, lints: I)
+    where
+        I: Iterator<Item = Lint>,
+    {
+        for lint in lints {
+            match lint.level {
+                Level::Error => self.error.push(lint),
+                _ => self.other.push(lint),
+            }
+        }
+    }
+
+    /// sort and dedup the internal lists
+    pub fn sort(&mut self) {
+        self.error.sort();
+        self.error.dedup();
+
+        self.other.sort();
+        self.other.dedup();
+    }
+
+    /// Return whether there are _any_ lints.
+    pub fn is_empty(&self) -> bool {
+        self.error.is_empty() && self.other.is_empty()
+    }
+}
+
+impl error::Error for Categorized {
+    fn description(&self) -> &str {
+        "multiple lint errors, both errors and warnings"
+    }
+}
+
+impl fmt::Display for Categorized {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "  ----- ERRORS -----:\n{}\n\n",
+            expect!(json::to_string_pretty(&self.error))
+        )?;
+        write!(
+            f,
+            "  ----- WARNINGS -----:\n{}\n",
+            expect!(json::to_string_pretty(&self.other))
+        )
+    }
+}
+
+impl Lint {
+    pub(crate) fn load_error<P: AsRef<Path>>(path: P, err: &str) -> Lint {
+        Lint {
+            level: Level::Error,
+            category: Category::LoadPaths,
+            path: Some(PathArc::new(path)),
+            line: None,
+            msg: format!("Error during loading: {}", err),
+        }
+    }
+
+    pub(crate) fn create_exists(err: String) -> Lint {
+        Lint {
+            level: Level::Error,
+            category: Category::CreateExists,
+            path: None,
+            line: None,
+            msg: err,
+        }
+    }
+
+    pub(crate) fn update_noop(err: String) -> Lint {
+        Lint {
+            level: Level::Error,
+            category: Category::UpdateNoop,
+            path: None,
+            line: None,
+            msg: err,
+        }
+    }
+
+    pub(crate) fn update_dne(err: String) -> Lint {
+        Lint {
+            level: Level::Error,
+            category: Category::UpdateDne,
+            path: None,
+            line: None,
+            msg: err,
+        }
+    }
+
+    pub(crate) fn delete_dne(err: String) -> Lint {
+        Lint {
+            level: Level::Error,
+            category: Category::DeleteDne,
+            path: None,
+            line: None,
+            msg: err,
+        }
+    }
+
+    pub(crate) fn id_overlap(err: String) -> Lint {
+        Lint {
+            level: Level::Error,
+            category: Category::IdOverlap,
+            path: None,
+            line: None,
+            msg: err,
+        }
+    }
+}
+
+pub(crate) fn io_error<P: AsRef<Path>>(lints: &Sender<Lint>, path: P, err: &str) {
+    lints
+        .send(Lint::load_error(path, err))
+        .expect("failed to send io-error");
+}
